1d651ad21e905eadf56e5e12338d6e285ced14d9
hassan el abdallah <hassankh613@gmail.com>
2023-08-25 23:50:38 +0300

    Add parser tests

---
+++ tests/query_parser_test.go
@@ -0,0 +1,54 @@
+package tests
+
+import (
+	"jsondb/internal/query_parser"
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	tests := []struct {
+		query  string
+		hasErr bool
+	}{
+		// Test cases for CREATE
+		{"create project 'project_name';", false},
+		{"create collection 'collection_name';", false},
+
+		// Test cases for DELETE
+		{"delete project 'project_name';", false},
+		{"delete collection 'collection_name';", false},
+
+		// Test cases for RENAME
+		{"rename project 'project_name' 'new_name';", false},
+		{"rename collection 'collection_name' 'new_name';", false},
+
+		// Test cases for ADD
+		{"add into 'collection_name' doc('attr1': 'value', 'attr2': 'value', 'attr3': doc( 'attr4' : 'value'));", false},
+
+		// Test cases for GET
+		{"get from 'collection_name' attrs('attr1', 'attr2') where('attr1' == 'attr2' || 'attr2' > 'attr4');", false},
+		{"get one from 'collection_name' attrs() where();", false},
+
+		// Test cases for UPDATE
+		{"update from 'collection_name' set('attr1': 'new_value', 'attr2': 'new_value') where();", false},
+
+		// Test cases for DELETE
+		{"delete from 'collection_name' where();", false},
+	}
+
+	for _, test := range tests {
+		lexer, _ := query_parser.CreateNewLexer(test.query)
+		parser := query_parser.CreateNewParser(*lexer)
+		parser.Parse()
+
+		if lexer.Err != nil {
+			t.Errorf("Query: %s\nExpected no error, but got: %v", test.query, parser.Err)
+		} else if parser.Err != nil && !test.hasErr {
+			t.Errorf("Query: %s\nExpected no error, but got: %v", test.query, parser.Err)
+		} else if parser.Err == nil && test.hasErr {
+			t.Errorf("Query: %s\nExpected error, but got none", test.query)
+		} else {
+			t.Logf(test.query, "OUTPUT:", parser.Seq.Tokens)
+		}
+	}
+}
