a2f6a4ba682228fb43e9c1a5ad833b5382d4ce45
hassan el abdallah <hassankh613@gmail.com>
2023-09-02 23:59:28 +0300

    Finishing interpreter

--- internal/query_interpreter/interpreter.go
+++ internal/query_interpreter/interpreter.go
@@ -47,11 +47,12 @@ func (i *Interpreter) Interpret() error {
 	case qp.ADD:
 		err = i.InterpretAdd()
 	case qp.GET:
-		return nil
+		i.InterpretGet()
 	case qp.UPDATE:
-		return nil
+		err = i.InterpretUpdate()
 	}
 
+	i.Query.Err = err
 	return err
 }
 
@@ -97,6 +98,11 @@ func (i *Interpreter) InterpretDelete() {
 
 	switch i.Seq.GetCurrentToken().Type {
 	case qp.FROM:
+		i.Query.OPT_TYPE = DELETE_DOCUMENT
+		i.Seq.Next()
+		i.Query.Kwargs["name"] = i.Seq.GetCurrentLexem()
+		i.Seq.Next()
+		i.InterpretWhere()
 		return
 	case qp.PROJECT:
 		i.Query.OPT_TYPE = DELETE_PROJECT
@@ -106,6 +112,124 @@ func (i *Interpreter) InterpretDelete() {
 
 	i.Seq.Next()
 	i.Query.Kwargs["name"] = i.Seq.GetCurrentLexem()
+
+}
+
+func (i *Interpreter) InterpretGet() {
+
+	i.Seq.Next()
+
+	switch i.Seq.GetCurrentToken().Type {
+
+	case qp.FROM:
+		i.Query.OPT_TYPE = GET_ALL_DOCUMENTS
+	case qp.ONE:
+		i.Query.OPT_TYPE = GET_ONE_DOCUMENT
+		i.Seq.Next()
+	}
+
+	i.Seq.Next()
+	i.Query.Kwargs["name"] = i.Seq.GetCurrentLexem()
+
+	i.Seq.Next()
+	i.InterpretAttrs()
+	i.Seq.Next()
+	i.InterpretWhere()
+
+}
+
+func (i *Interpreter) InterpretUpdate() error {
+
+	i.Query.OPT_TYPE = UPDATE_DOCUMENT
+	i.Seq.Next()
+	i.Seq.Next()
+
+	i.Query.Kwargs["name"] = i.Seq.GetCurrentLexem()
+
+	i.Seq.Next()
+	i.Seq.Next()
+
+	set := []string{}
+	values := []interface{}{}
+
+	for i.Seq.GetCurrentToken().Type != qp.CLOSE_PARAM {
+
+		i.Seq.Next()
+		set = append(set, i.Seq.GetCurrentLexem())
+		i.Seq.Next()
+		if i.Seq.GetCurrentToken().Type == qp.CLOSE_PARAM {
+			values = append(values, nil)
+			break
+		}
+		if i.Seq.GetCurrentToken().Type == qp.COMMA {
+			values = append(values, nil)
+			continue
+		}
+		i.Seq.Next()
+
+		if i.Seq.GetCurrentToken().Type == qp.NUMBER_LITERAL {
+
+			if n, err := strconv.Atoi(i.Seq.GetCurrentLexem()); err == nil {
+				values = append(values, n)
+			} else if n2, err := strconv.ParseFloat(i.Seq.GetCurrentLexem(), 64); err == nil {
+				values = append(values, n2)
+			} else {
+				return errors.New("INTERPRETER ERROR: invalid number '" + i.Seq.GetCurrentLexem() + "'")
+
+			}
+
+		} else if i.Seq.GetCurrentToken().Type == qp.TRUE {
+			values = append(values, true)
+		} else if i.Seq.GetCurrentToken().Type == qp.FALSE {
+			values = append(values, false)
+		} else if i.Seq.GetCurrentToken().Type == qp.NULL {
+			values = append(values, nil)
+		} else {
+			values = append(values, i.Seq.GetCurrentLexem())
+		}
+		i.Seq.Next()
+	}
+
+	i.Query.Kwargs["set"] = set
+	i.Query.Kwargs["values"] = values
+	i.Seq.Next()
+	i.InterpretWhere()
+
+	return nil
+
+}
+
+func (i *Interpreter) InterpretAttrs() {
+
+	i.Seq.Next()
+	i.Seq.Next()
+	attrs := []string{}
+
+	for i.Seq.GetCurrentToken().Type != qp.CLOSE_PARAM {
+		attrs = append(attrs, i.Seq.GetCurrentLexem())
+		i.Seq.Next()
+		if i.Seq.GetCurrentToken().Type == qp.CLOSE_PARAM {
+			break
+		}
+		i.Seq.Next()
+	}
+
+	i.Query.Kwargs["attrs"] = attrs
+
+}
+
+func (i *Interpreter) InterpretWhere() {
+
+	i.Seq.Next()
+	i.Seq.Next()
+
+	where := []string{}
+
+	for i.Seq.GetCurrentToken().Type != qp.CLOSE_PARAM {
+		where = append(where, i.Seq.GetCurrentLexem())
+		i.Seq.Next()
+	}
+	i.Query.Kwargs["where"] = where
 
 }
 
