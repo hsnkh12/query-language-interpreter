efa469e2d6d8c17e7602172ac256d214d6d756f8
hassan el abdallah <hassankh613@gmail.com>
2023-09-05 21:52:07 +0300

    Adding updates

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
-	"jsondb/internal/query_interpreter"
-	"jsondb/internal/query_parser"
+	dm "jsondb/internal/database_manager"
+	"os"
 )
 
 func main() {
 
-	src := `update from 'collection_name' set('attr1', 'attr2': 'new_value') where();`
+	scanner := bufio.NewScanner(os.Stdin)
 
-	lexer, err := query_parser.CreateNewLexer(src)
+	for {
+		fmt.Print("query> ")
 
-	if err != nil {
-		panic(err)
+		scanner.Scan()
+		src := scanner.Text()
+
+		if src == "q" {
+			break
+		}
+
+		dbManager := dm.CreateNewDatabaseManager(src, "project1", "/Users/hassanelabdallah/golang/pracs/json_dbms/configs/data-sample")
+
+		err := dbManager.ExecuteQuery()
+
+		if err != nil {
+			panic(err)
+		}
+
 	}
-
-	parser := query_parser.CreateNewParser(*lexer)
-
-	interpreter := query_interpreter.CreateNewInterpreter(*parser)
-
-	err = interpreter.Interpret()
-
-	if err != nil {
-		panic(err)
-	}
-
-	q, _ := json.Marshal(interpreter.Query)
-
-	fmt.Println(string(q))
-
 }
